919ce7be0af877aa2638316512526391ae989cce
Jan-Henrik Damaschke <jdamaschke@visorian.com>
2021-03-23 19:52:48 +0100

    refactor(app): :art:  Added labels for animation classes

--- components/Animation.vue
+++ components/Animation.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="flex gap-4 w-full justify-center mt-12">
-    <div class="pulse-class"></div>
-    <div
-      class="relative w-40 h-40 rounded-full bg-teal-600 opacity-80 animate-pulse"
-    ></div>
+    <div class="w-40">
+      <div class="pulse-class"></div>
+      <p class="text-center mt-2">class="pulse-class" (defined with @apply)</p>
+    </div>
+    <div class="w-40">
+      <div
+        class="relative w-40 h-40 rounded-full bg-teal-600 opacity-80 animate-pulse"
+      ></div>
+      <p class="text-center mt-2">
+        class="relative w-40 h-40 rounded-full bg-teal-600 opacity-80
+        animate-pulse"
+      </p>
+    </div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title text-center">nuxt-windicss</h1>
+      <h1 class="title text-center">Windi CSS animations</h1>
       <Animation />
     </div>
   </div>
